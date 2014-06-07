a142c6ebe8de1231ea3d3b42576f87f6f2f34c03
mattimatti <mmonti@gmail.com>
2014-06-07 19:56:55 +0200

    changed main

--- js/main.js
+++ js/main.js
@@ -27,12 +27,12 @@ require.config({
 	
 
 	paths: {
-		jquery: 'vendor/jquery-1.9.1',
-		skrollr: 'vendor/skrollr.min',
-		tweenmax: 'vendor/TweenMax',
-		reel: 'vendor/jquery.reel',
-		panzoom: 'vendor/jquery.panzoom',
-		videojs: 'vendor/video-js-4.1.0/video.dev'
+		jquery: '../vendor/jquery-1.9.1',
+		skrollr: '../vendor/skrollr.min',
+		tweenmax: '../vendor/TweenMax',
+		reel: '../vendor/jquery.reel',
+		panzoom: '../vendor/jquery.panzoom',
+		videojs: '../vendor/video-js-4.1.0/video.dev'
 	}
 
 });
