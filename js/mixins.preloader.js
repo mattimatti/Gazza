6752d0a3c30c4f485ede30e50ebd574f331a5d18
mattimatti <mmonti@gmail.com>
2014-06-07 09:45:40 +0200

    modifiche preloader

--- js/mixins.preloader.js
+++ js/mixins.preloader.js
@@ -13,24 +13,80 @@
 				return;
 			}
 
-			if(this.preloaded){
+			if (this.preloaded) {
 				this.initialize();
 				return;
 			}
 
-			this.$el.find('*').each(function(index, element) {
-				var item = $(element);
-				if(item.attr("data-src") && item.attr("data-src") !== ''){
-					item.attr("src" , item.attr("data-src"));
-				}
-			});
+			// for each element
+			this.$el.find('*').each($.proxy(this.eachImage, this));
 
 			this.preloaded = true;
 
 
+		},
+
+
+		// iterate all elements
+		eachImage: function(index, element) {
+			var item = $(element);
+			if (item.attr("data-src") && item.attr("data-src") !== '') {
+				this.preloadElement(item);
+			}
+		},
+
+
+		preloadElement: function(item) {
+			console.error('preloadElement', item);
+
+			item.attr("src", item.attr("data-src"));
+
+			var enableCallbacks = true;
+			var timeout = null;
+			var self = this;
+
+			console.error('load Ajax', item.attr("data-src"));
+
+			// $.ajax({
+			// 	url:item.attr("data-src"),
+			// 	beforeSend: function(xhr) {
+			// 		timeout = setTimeout(function() {
+			// 			xhr.abort();
+			// 			enableCallbacks = false;
+			// 			// Handle the timeout
+
+			// 		}, 5000);
+			// 	},
+			// 	error: function(xhr, textStatus, errorThrown) {
+			// 		clearTimeout(timeout);
+			// 		if (!enableCallbacks) {
+			// 			return;
+			// 		}
+			// 	},
+			// 	success: function(data, textStatus) {
+			// 		clearTimeout(timeout);
+			// 		if (!enableCallbacks) {
+			// 			return;
+			// 		}
+
+			// 	}
+			// });
+
+		},
+
+		// once one element is loaded
+		loadedElement: function(){
+			console.error('loadedElement', arguments);
+		},
+
+
+		stopPreload: function() {
+			console.error('stopPreload', arguments);
+
 		}
 
-	}
+
+	};
 
 
 	window.MixinPreloader = MixinPreloader;
